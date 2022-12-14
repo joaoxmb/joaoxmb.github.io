7bcdb785f22ddb636418d5ff2428a388d63beaf9
jaoxmb <112896260+jaoxmb@users.noreply.github.com>
2022-12-14 01:43:28 -0300

    Add files via upload

---
+++ gerador de minutas/script.js
@@ -0,0 +1,43 @@
+const dinheiro = ( value ) => value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+const abrirMinuta = () => {
+  let popUp = window.open(`/minuta/index.html`, "teste", "left=100,top=100,width=320,height=600");
+      try {
+          popUp.focus();
+      }catch (e) {
+          alert("Pop-up Blocker is enabled! Please disable your pop-up blocker.");
+      }
+};
+
+minuta.iniciar();
+minuta.load.diarias();
+minuta.load.tabela();
+
+
+
+$('input[type="dinheiro"]').inputmask('currency', {
+  "autoUnmask": true,
+  radixPoint: ",",
+  groupSeparator: ".",
+  allowMinus: false,
+  prefix: 'R$ ',
+  digits: 2,
+  digitsOptional: false,
+  rightAlign: true,
+  unmaskAsNumber: true,
+});
+
+$('input[type="numero"]').inputmask('decimal', {
+  "autoUnmask": true,
+  allowMinus: false,
+  rightAlign: true,
+  unmaskAsNumber: true,
+});
+
+$('.input').click(function (event) {
+  $(this).find('input,textarea,select').focus();
+});
+
+$('textarea').on('input', data =>{
+  data.target.style.height = 'auto';
+  data.target.style.height = (data.target.scrollHeight) + 'px';
+});
