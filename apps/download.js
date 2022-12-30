8525c66c58f3720a90bd053bfefb3cfd4101a319
jaoxmb <112896260+jaoxmb@users.noreply.github.com>
2022-12-29 21:50:19 -0300

    Add files via upload

---
+++ apps/download.js
@@ -0,0 +1,22 @@
+const download = {
+  fechar: () => {
+    $('#aba-download').css({'top': '100%'});
+    $('#aba-inicio').css({transform: 'translate(-50%, -0%) scale(1)',filter: 'brightness(1)', padding: '0', overflow: 'unset'});
+    $('body').css({background: ''});
+
+    setTimeout(function() {
+      $('#aba-download').css('display', 'none');
+    }, 200);
+  },
+
+  compartilhar: async () => {
+    const base64url = $('.image-minuta').attr('src')
+    const blob = await (await fetch(base64url)).blob();
+    const file = new File([blob], 'fileName.png', { type: blob.type });
+    navigator.share({
+      title: 'Hello',
+      text: 'Check out this image!',
+      files: [file],
+    });
+  }
+};
