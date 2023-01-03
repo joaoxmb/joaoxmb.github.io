2f6a4fb0c3875f3ae17216edacf41bbd1ac16659
jaoxmb <112896260+jaoxmb@users.noreply.github.com>
2023-01-03 19:55:51 +0000

    s

--- minuta/share.js
+++ minuta/share.js
@@ -18,7 +18,7 @@ const imprimir = () => {
 const compartilhar = async () =>{
   if(navigator.share){
     const blob = await (await fetch(IMAGE)).blob();
-    const file = new File([blob], nomeArquivo.archive(), { type: 'image/png' });
+    const file = new File([blob], { type: 'image/png' });
     navigator.share({
       files: [file],
     });
